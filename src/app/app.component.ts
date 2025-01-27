5a3a3f80a1a9e69c67d492b966a771bb56f00b63
Keith <karirukeith@gmail.com>
2025-01-27 19:14:29 +0300

    latest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from './navbar/navbar.component';
+import * as AOS from 'aos';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'anjeka';
+
+  ngOnInit() {
+    AOS.init();
+  }
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,57 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutPageComponent} from './about-page/about-page.component';
+import {ServicesPageComponent} from './services-page/services-page.component';
+import {CareersPageComponent} from './careers-page/careers-page.component';
+import {TeamPageComponent} from './team-page/team-page.component';
+import {BlogPageComponent} from './blog-page/blog-page.component';
+import {ContactPageComponent} from './contact-page/contact-page.component';
+import {ServicesDetailsComponent} from './services-details/services-details.component';
+import {NgModule} from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  {
+    path: 'services',
+    component: ServicesPageComponent
+  },
+  {
+    path: 'careers',
+    component: CareersPageComponent
+  },
+  {
+    path: 'team',
+    component: TeamPageComponent
+  },
+  {
+    path: 'blog',
+    component: BlogPageComponent
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent
+  },
+  {
+    path: 'services/:id',
+    component: ServicesDetailsComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled', // Automatically scrolls to the top on navigation
+    }),
+  ],
+  exports: [RouterModule],
+})
+
+export class AppRoutingModule {}
+
